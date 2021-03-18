04e6bd8520ea67be5192e3929de89a67b1ce23f5
SteffenThie <steffenthie08@gmail.com>
2021-03-18 14:30:30 +0100

    init

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import brickBraker.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Game spiel1 = new Game();
+        spiel1.makeGame();
+        
+    }
+}
---
+++ src/brickBraker/Game.java
@@ -0,0 +1,23 @@
+package brickBraker;
+
+import javax.swing.JFrame;
+
+
+
+public class Game {
+
+public void makeGame() {
+    
+    JFrame game = new JFrame();
+    game.setBounds(10, 10, 700, 600);
+    game.setTitle("Breakout Ball"); 
+    game.setResizable(false); 
+    game.setVisible(true);
+
+}
+    
+     
+
+
+
+}
